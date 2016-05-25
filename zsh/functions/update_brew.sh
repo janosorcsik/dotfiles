6ef9ff3026a20a495537441be5a9bfbb5e03e9ab
János Orcsik <orcsikj@gmail.com>
2016-05-25 17:49:42 +0200

    Fix au alias

--- zsh/functions/update_brew.sh
+++ zsh/functions/update_brew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mas upgrade
 brew update
 brew upgrade --all
 
@@ -7,6 +8,9 @@ for c in `brew cask list`; do
   ! brew cask info $c | grep -qF "Not installed" || brew cask install --force $c
 done
 
+for c in /opt/homebrew-cask/Caskroom/*; do vl=(`ls -t $c`) && for v in "${vl[@]:1}"; do sudo rm -rf "$c/$v"; done; done
+
 brew cleanup
+brew prune
 brew cask cleanup
 brew bundle dump --force
