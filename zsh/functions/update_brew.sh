4816d771212429168f6bde7a38a1a59b4484a83d
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2016-10-10 21:29:53 +0200

    Shellchecked

--- zsh/functions/update_brew.sh
+++ zsh/functions/update_brew.sh
@@ -4,11 +4,11 @@ brew update
 brew upgrade
 
 brew cask update
-for c in `brew cask list`; do
-  ! brew cask info $c | grep -qF "Not installed" || brew cask install --force $c
+for c in $(brew cask list); do
+  ! brew cask info "$c" | grep -qF "Not installed" || brew cask install --force "$c"
 done
 
-for c in /opt/homebrew-cask/Caskroom/*; do vl=(`ls -t $c`) && for v in "${vl[@]:1}"; do sudo rm -rf "$c/$v"; done; done
+for c in /opt/homebrew-cask/Caskroom/*; do vl=($(ls -t "$c")) && for v in "${vl[@]:1}"; do sudo rm -rf "$c/$v"; done; done
 
 brew cleanup
 brew prune
