643ef212cb673695b58ceec3bb6d1d0b1ca43ece
János Orcsik <orcsikj@gmail.com>
2016-11-01 10:53:12 +0100

    Fix app linking

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ ln -s $PWD/Brewfile ~/Brewfile
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/bundle
 brew bundle
-brew linkapps mpv
+brew linkapps
 
 # Reload QuickLook
 qlmanage -r
