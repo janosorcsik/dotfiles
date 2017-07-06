7465efd0916cdffeeafcc004555aed66dab06752
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2017-07-06 23:30:59 +0200

    aa

--- zsh/other.zsh
+++ zsh/other.zsh
@@ -71,22 +71,22 @@ _force_rehash() {
 }
 
 # forces zsh to realize new commands
-zstyle ':completion:*' completer _oldlist _expand _force_rehash _complete _match
+zstyle ':completion:*' completer _oldlist _expand _complete _match _ignored _approximate
+
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # colored completion - use my LS_COLORS
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-# more completers to use
-zstyle ':completion:*' _ignored _approximate
-
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+# rehash if command not found (possibly recently installed)
+zstyle ':completion:*' rehash true
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
 # menu if nb items > 2
-zstyle ':completion:*:default' menu select=2
+zstyle ':completion:*' menu select=2
 
 zstyle ":completion:*:descriptions" format "%B%d%b"
 zstyle ':completion:*' group-name ''
