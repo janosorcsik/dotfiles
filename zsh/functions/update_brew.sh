f9a0cebaf649fa44ce2f2a94ea7bb21bfc5eed0b
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2018-03-28 20:47:05 +0200

    Better cleanup

--- zsh/functions/update_brew.sh
+++ zsh/functions/update_brew.sh
@@ -4,11 +4,12 @@ mas upgrade
 brew update
 brew upgrade
 
-brew cu --yes --cleanup --quiet
+brew cu --yes --cleanup --quiet --no-brew-update
 
-brew bundle dump --force
-
-brew cleanup
-brew cask cleanup
+brew bundle dump --global --force
 
 brew prune
+
+brew cleanup --force -s
+brew cask cleanup
+rm -rf `brew --cache`
