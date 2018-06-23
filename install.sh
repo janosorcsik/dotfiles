2d89deeeedcb31928190b37a7117287cba901e2e
János Orcsik <orcsikj@gmail.com>
2018-06-23 22:12:13 +0200

    Speed up ZSH

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -ux
 
 echo 'Start installing...'
 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications --fontdir=/Library/Fonts"
 
 echo "Installing HomeBrew and apps..."
 ln -s ~/.dotfiles/Brewfile ~/.Brewfile
@@ -19,8 +19,6 @@ sudo rm ~/.zshrc
 echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
 ln -s ~/.dotfiles/zshrc ~/.zshrc
-sudo rm ~/.zsh_plugins.txt
-ln -s ~/.dotfiles/zsh_plugins.txt ~/.zsh_plugins.txt
 sudo rm -rf ~/.zsh
 mkdir ~/.zsh
 ln -s ~/.dotfiles/zsh/* ~/.zsh/
