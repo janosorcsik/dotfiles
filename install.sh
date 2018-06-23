c278351e805cb45164b2f8d4fc6acb18760f2bad
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2018-06-23 21:09:33 +0200

    Sapceship prompt from NPM

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ sudo rm ~/.gitconfig
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 
 echo "NPM..."
-sudo npm install -g eslint
+sudo npm install -g eslint spaceship-prompt
 
 echo "Skype..."
 sudo rm ~/Library/Application\ Support/Skype/orcsik.janos/main.db
