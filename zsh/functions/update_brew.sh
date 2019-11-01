bc4ef6271cd9a8d53b50ae61002636c033e08f84
János Orcsik <orcsikj@gmail.com>
2019-11-01 20:46:21 +0100

    Dump after clean

--- zsh/functions/update_brew.sh
+++ zsh/functions/update_brew.sh
@@ -6,7 +6,7 @@ brew upgrade --fetch-HEAD
 
 brew cu --all --yes --quiet --no-brew-update
 
-brew bundle dump --global --force
-
 brew cleanup -s
 rm -rf "$(brew --cache)"
+
+brew bundle dump --global --force --describe
