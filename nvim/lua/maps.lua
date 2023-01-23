295e5a185b05163a16e049b1738a9db1e4f48db5
János Orcsik <orcsikj@gmail.com>
2023-01-23 19:00:47 +0100

    Add shortcuts to moving in buffer

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,7 +1,7 @@
-vim.keymap.set("n", "<leader>e", require("netrw").toggle_netrw)
+vim.keymap.set("n", "<leader>e", require("netrw").toggle_netrw, { desc = "Toggle Netrw" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -9,7 +9,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>bn", require("netrw").toggle_netrw)
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 
 -- Telescope
 local tb = require("telescope.builtin")
