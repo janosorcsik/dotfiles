2d873c9c83053bcd16e374213c1cf28ddb074de1
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2023-04-05 21:52:21 +0200

    Use stable branch for lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,16 +4,16 @@ vim.g.mapleader = " "
 -- Install Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
