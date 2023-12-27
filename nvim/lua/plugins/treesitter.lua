f1a82407612f97338e2faac3aae7e6c5dff50acc
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2023-12-27 12:24:52 +0100

    fix treesitter config after breaking change

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ require("nvim-treesitter.configs").setup({
 	sync_install = false,
 	highlight = { enable = true, additional_vim_regex_highlighting = false },
 	indent = { enable = true },
-	context_commentstring = { enable = true, enable_autocmd = false },
+	enable_autocmd = false,
 	ensure_installed = {
 		"bash",
 		"c_sharp",
