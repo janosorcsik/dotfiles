0c4c4c7ff6d47940b4a2d80025330db5d1dde18d
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-04-22 13:13:28 +0200

    Add incsearch

--- config/nvim/lua/set.lua
+++ config/nvim/lua/set.lua
@@ -25,6 +25,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.incsearch = true
 
 vim.opt.cursorline = true
 vim.opt.mouse = "a"
