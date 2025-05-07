885561af63b42c5a3d00b8f0d5ce87bb313cdbd1
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-05-07 22:19:20 +0200

    Add comments to completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -10,13 +10,18 @@ fi
 FPATH="$BREW_PREFIX/share/zsh/site-functions:$BREW_PREFIX/share/zsh-completions:${FPATH}"
 
 # Completion styling
-zstyle ':completion:*' use-cache true
-zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*:*:*:default' menu yes select
+# Completion styling settings
+zstyle ':completion:*' use-cache true  # Enable caching for completion results to speed up repeated completions
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'  # Enable case-insensitive matching for completion
+zstyle ':completion:*:*:*:default' menu yes select  # Show a menu for completions and allow selecting one with the arrow keys
+
+# Completion formatting
+zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'  # Customize the message format for completion
+zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d --\e[0m'  # Format description lines to highlight them
+zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"  # Set the color scheme for completion listing
+
+# Group completions under a common name (empty here, can be customized)
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d --\e[0m'
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
 
 # Carapace completions
 export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
