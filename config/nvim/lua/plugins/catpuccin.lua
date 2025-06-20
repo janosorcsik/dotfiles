024fe471e4dee6c4384f34bc4c47dba3cde9b5bc
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-06-20 07:55:26 +0200

    Enable transparent background in Neovim

--- config/nvim/lua/plugins/catpuccin.lua
+++ config/nvim/lua/plugins/catpuccin.lua
@@ -4,9 +4,11 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
+			transparent_background = true,
 			integrations = {
 				blink_cmp = true,
 				mason = true,
+				snacks = true,
 			},
 		})
 
