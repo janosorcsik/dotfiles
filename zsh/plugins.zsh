bcecb443753745146c06467eb03901748dd889c9
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2025-08-20 14:26:37 +0200

    Remove zsh-nvm

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,17 +1,10 @@
 # Source plugins - keep at the end for best performance
-source "$BREW_PREFIX/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
-source "$BREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source ~/.catppuccin_mocha-zsh-syntax-highlighting.zsh
-source "$BREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # FZF configuration
+source <(fzf --zsh)
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_CTRL_T_OPTS="--preview='bat --style numbers,changes --color=always {} | head -500' --bind ctrl-d:preview-page-down,ctrl-u:preview-page-up --no-height --no-reverse"
-
-# NVM configuration - lazy loading for performance
-export NVM_AUTO_USE=true
-export NVM_COMPLETION=true
-export NVM_LAZY_LOAD=true
-source ~/.zsh-nvm/zsh-nvm.plugin.zsh
 
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
