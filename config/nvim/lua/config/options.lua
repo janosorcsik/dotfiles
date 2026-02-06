4ddbeb66df29da9b7cfd3de425dc561c8058cc9f
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2026-02-06 14:59:28 +0100

    Organize nvim options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,27 +1,24 @@
+-- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Editor
 vim.opt.showmode = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+vim.opt.smartindent = true
+vim.opt.confirm = true
+
+-- System
+vim.opt.swapfile = false
+vim.opt.undofile = true
 
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
-
-vim.opt.swapfile = false
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.scrolloff = 8
-
-vim.opt.confirm = true
-
-vim.opt.cursorline = true
-
-vim.opt.signcolumn = "yes"
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.undofile = true
