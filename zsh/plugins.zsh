c68781823ac31b5db754fcfc4a27348bc6af8ce8
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2026-02-06 15:10:57 +0100

    Remove duplicated fzf loading

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,7 +4,6 @@ source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 # FZF configuration
 source <(fzf --zsh)
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_CTRL_T_OPTS="--preview='bat --style numbers,changes --color=always {} | head -500' --bind ctrl-d:preview-page-down,ctrl-u:preview-page-up --no-height --no-reverse"
 
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
