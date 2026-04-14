f4987a56586764ced59176c065a3d57bfb02ede0
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2026-04-14 19:51:58 +0200

    Remove redudant setting

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -4,7 +4,6 @@ HISTSIZE=5000                    # How many lines of history to keep in memory
 SAVEHIST=$HISTSIZE               # Number of history entries to save to disk
 setopt BANG_HIST                 # Treat the '!' character specially during expansion
 setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits
 setopt SHARE_HISTORY             # Share history between all sessions
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history
 setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again
