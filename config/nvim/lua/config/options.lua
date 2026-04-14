8a05fde016d15fa0cc333c6ebb3ec8fdfbceddfe
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2026-04-14 19:52:39 +0200

    Organize options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
+-- Appearance
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -5,15 +12,14 @@ vim.opt.relativenumber = true
 -- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.showmatch = true
 
--- Editor
-vim.opt.showmode = false
-vim.opt.cursorline = true
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff = 8
-vim.opt.wrap = false
+-- Editing
 vim.opt.smartindent = true
 vim.opt.confirm = true
+vim.opt.scrolloff = 8
 
 -- System
 vim.opt.swapfile = false
