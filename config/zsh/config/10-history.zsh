29c8626626c833e7eff3344f0e44380fea6176e3
János Orcsik <4984853+janosorcsik@users.noreply.github.com>
2026-09-23 15:27:49 +0200

    Remove HIST_EXPIRE_DUPS_FIRST option from zsh history config

--- config/zsh/config/10-history.zsh
+++ config/zsh/config/10-history.zsh
@@ -7,7 +7,6 @@ mkdir -p "${HISTFILE:h}"                 # zsh won't create the parent directory
 setopt BANG_HIST                         # Treat the '!' character specially during expansion
 setopt EXTENDED_HISTORY                  # Write the history file in the ":start:elapsed;command" format
 setopt SHARE_HISTORY                     # Share history between all sessions
-setopt HIST_EXPIRE_DUPS_FIRST            # Expire duplicate entries first when trimming history
 setopt HIST_IGNORE_ALL_DUPS              # Delete old recorded entry if new entry is a duplicate
 setopt HIST_FIND_NO_DUPS                 # Do not display a line previously found
 setopt HIST_IGNORE_SPACE                 # Don't record an entry starting with a space
